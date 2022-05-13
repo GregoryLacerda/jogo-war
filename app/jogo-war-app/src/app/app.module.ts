563659790f7833bb82b49cce9c32e6be118e65f9
Gregory Lacerda Batista <gregorylacerda@hotmail.com>
2022-05-13 02:39:17 -0300

    Criado os componentes Jogador e Partida

--- app/jogo-war-app/src/app/app.module.ts
+++ app/jogo-war-app/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { JogadorComponent } from './compenents/jogador/jogador.component';
+import { PartidaComponent } from './compenents/partida/partida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JogadorComponent,
+    PartidaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
